23728a240e3d5c8d2848917ad3249eaffd0848bc
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-06-18 23:00:01 +0200

    Extend String class to get files name

--- app/src/main/java/org/gmetais/downloadmanager/Structures.kt
+++ app/src/main/java/org/gmetais/downloadmanager/Structures.kt
@@ -13,3 +13,9 @@ data class User(val Id: String,
                 val Name: String)
 data class RequestBody(val Path: String,
                        val User: String)
+
+fun  String.getNameFromPath(): String {
+    var trailing = this.endsWith('/')
+    var index = if (!trailing) this.lastIndexOf('/') else this.substring(0, this.length-2).lastIndexOf('/')
+    return this.substring(index+1, if (trailing) this.length-1 else this.length)
+}
