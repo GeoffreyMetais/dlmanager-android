570888892e03f3fcec1245c016a7c410a1ff9403
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-07-02 02:14:22 +0200

    DataBinding for link creator

--- app/src/main/java/org/gmetais/downloadmanager/LinkCreatorDialog.kt
+++ app/src/main/java/org/gmetais/downloadmanager/LinkCreatorDialog.kt
@@ -7,41 +7,39 @@ import android.support.design.widget.Snackbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
+import org.gmetais.downloadmanager.databinding.DialogLinkCreatorBinding
 import org.gmetais.downloadmanager.model.SharesListModel
 
-class LinkCreatorDialog : BottomSheetDialogFragment(), View.OnClickListener {
+class LinkCreatorDialog : BottomSheetDialogFragment() {
 
-    val mTitle by bind<TextView>(R.id.title)
-    val mButton by bind<Button>(R.id.link_action)
-    val mEditText by bind<EditText>(R.id.edit_name)
-    lateinit var mPath : String
+    val mPath : String by lazy {arguments.getString("path")}
     val shares: SharesListModel by lazy { ViewModelProviders.of(activity).get(SharesListModel::class.java) }
+    lateinit var mBinding: DialogLinkCreatorBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.dialog_link_creator, container, false)
+        mBinding = DialogLinkCreatorBinding.inflate(inflater, container, false)
+        return mBinding.root
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mPath = arguments.getString("path")
-        mTitle?.text = getString(R.string.create_download_link_for, mPath.getNameFromPath())
-        mButton?.setOnClickListener(this)
+        mBinding.title = mPath.getNameFromPath()
+        mBinding.hanler = ClickHandler()
     }
 
-    override fun onClick(view: View?) {
-        val file = SharedFile(path = mPath, name = mEditText?.text.toString())
-        async(CommonPool) {
-            if (RequestManager.add(file).isSuccessful) {
-                shares.invalidate()
-                dismiss()
-            } else
-                Snackbar.make(view!!, "failure", Snackbar.LENGTH_LONG).show()
+    inner class ClickHandler {
+        fun onClick(view: View?) {
+            val file = SharedFile(path = mPath, name = mBinding.editName.text.toString())
+            async(CommonPool) {
+                if (RequestManager.add(file).isSuccessful) {
+                    shares.invalidate()
+                    dismiss()
+                } else
+                    Snackbar.make(view!!, "failure", Snackbar.LENGTH_LONG).show()
 
+            }
         }
     }
 }
