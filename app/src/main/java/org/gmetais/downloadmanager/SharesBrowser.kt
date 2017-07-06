6645943ceb6cc9391a04f3f969c99910e28aa0e6
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-07-06 18:17:41 +0200

    Show progress while loading shares

--- app/src/main/java/org/gmetais/downloadmanager/SharesBrowser.kt
+++ app/src/main/java/org/gmetais/downloadmanager/SharesBrowser.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
@@ -18,6 +19,7 @@ import org.gmetais.downloadmanager.model.SharesListModel
 class SharesBrowser : LifecycleFragment(), SharesAdapter.ShareHandler {
 
     private lateinit var mBinding: BrowserBinding
+    private val mHandler: Handler by lazy { Handler() }
     val shares: SharesListModel by lazy { ViewModelProviders.of(activity).get(SharesListModel::class.java) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -31,10 +33,12 @@ class SharesBrowser : LifecycleFragment(), SharesAdapter.ShareHandler {
         mBinding.filesList.addItemDecoration(DividerItemDecoration(mBinding.filesList.context, DividerItemDecoration.VERTICAL))
         mBinding.filesList.adapter = SharesAdapter(this)
         shares.shares.observe(this, Observer<MutableList<SharedFile>> { update(it!!) })
+        activity.title = "Shares"
+        showProgress()
     }
 
     private fun update(shares: MutableList<SharedFile>) {
-        activity.title = "Shares"
+        showProgress(false)
         (mBinding.filesList.adapter as SharesAdapter).update(shares)
     }
 
@@ -49,11 +53,21 @@ class SharesBrowser : LifecycleFragment(), SharesAdapter.ShareHandler {
     }
 
     override fun delete(share: SharedFile) {
+        showProgress()
         shares.delete(share)
     }
 
     fun onDelResponse(success: Boolean) {
         if (!success)
             Snackbar.make(mBinding.root, "failure", Snackbar.LENGTH_LONG).show()
+    }
+
+    fun showProgress(show: Boolean = true) {
+        if (show)
+            mHandler.postDelayed({mBinding.sharesProgress.visibility = View.VISIBLE}, 300)
+        else {
+            mHandler.removeCallbacksAndMessages(null)
+            mBinding.sharesProgress.visibility = View.GONE
+        }
     }
 }
