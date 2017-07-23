8d9382794d26c2960f488496b5d2e94e1cdd3113
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-07-23 20:07:01 +0200

    Thread DiffUtil calculation

--- app/src/main/java/org/gmetais/downloadmanager/adapters/DiffUtilAdapter.kt
+++ app/src/main/java/org/gmetais/downloadmanager/adapters/DiffUtilAdapter.kt
@@ -1,17 +1,42 @@
 package org.gmetais.downloadmanager.adapters
 
+import android.support.annotation.MainThread
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import java.util.*
 
 
 abstract class DiffUtilAdapter<D, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
 
-    var mDataset: List<D> = listOf()
+    protected var mDataset: List<D> = listOf()
+    private val mPendingUpdates = ArrayDeque<List<D>>()
 
+    @MainThread
     fun update (list: List<D>) {
-        val result = DiffUtil.calculateDiff(DiffCallback(list), false)
-        mDataset = list
-        result.dispatchUpdatesTo(this@DiffUtilAdapter)
+        mPendingUpdates.add(list)
+        if (mPendingUpdates.size == 1)
+            internalUpdate(list)
+    }
+
+    @Suppress("EXPERIMENTAL_FEATURE_WARNING")
+    private fun internalUpdate(list: List<D>) {
+        launch(CommonPool) {
+            val result = DiffUtil.calculateDiff(DiffCallback(list), false)
+            launch(UI) {
+                mDataset = list
+                result.dispatchUpdatesTo(this@DiffUtilAdapter)
+                processQueue()
+            }
+        }
+    }
+
+    private fun processQueue() {
+        mPendingUpdates.remove()
+        if (!mPendingUpdates.isEmpty())
+            internalUpdate(mPendingUpdates.peek())
     }
 
     inner class DiffCallback(val newList: List<D>) : DiffUtil.Callback() {
