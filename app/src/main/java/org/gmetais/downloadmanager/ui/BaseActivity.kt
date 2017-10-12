f4430ee777a9462f96035d68033061d3ccc2e318
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-10-12 13:58:12 +0200

    Just to play with observable delegate

--- app/src/main/java/org/gmetais/downloadmanager/ui/BaseActivity.kt
+++ app/src/main/java/org/gmetais/downloadmanager/ui/BaseActivity.kt
@@ -8,10 +8,11 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import org.gmetais.downloadmanager.NetworkHelper
 import org.gmetais.downloadmanager.R
+import kotlin.properties.Delegates
 
 @SuppressLint("Registered")
 open class BaseActivity : AppCompatActivity() {
-    private var mAlertDialog : AlertDialog? = null
+    private var mAlertDialog by Delegates.observable<AlertDialog?>(null) { _, _, new -> new?.show() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +37,6 @@ open class BaseActivity : AppCompatActivity() {
                         .setMessage(getString(R.string.dialog_network_message))
                         .setPositiveButton(getString(android.R.string.ok), { dialog, _ -> dialog.dismiss() })
                         .create()
-            mAlertDialog!!.show()
         } else if (showing)
             mAlertDialog!!.dismiss()
     }
