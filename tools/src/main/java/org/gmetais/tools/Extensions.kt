586afb1fd3268f3e90a8a973b395510d69920b17
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-09-16 21:15:50 +0200

    CoroutineScope extensions

---
+++ tools/src/main/java/org/gmetais/tools/Extensions.kt
@@ -0,0 +1,33 @@
+package org.gmetais.tools
+
+import android.arch.lifecycle.GenericLifecycleObserver
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleOwner
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.Main
+import kotlinx.coroutines.experimental.launch
+
+fun LifecycleOwner.createJob(cancelEvent: Lifecycle.Event = Lifecycle.Event.ON_STOP): Job = Job().also { job ->
+    lifecycle.addObserver(object : GenericLifecycleObserver {
+        override fun onStateChanged(source: LifecycleOwner?, event: Lifecycle.Event) {
+            if (event == cancelEvent) {
+                lifecycle.removeObserver(this)
+                job.cancel()
+            }
+        }
+    })
+}
+
+private val lifecycleCoroutineScopes = mutableMapOf<Lifecycle, CoroutineScope>()
+
+val LifecycleOwner.coroutineScope: CoroutineScope
+    get() = lifecycleCoroutineScopes[lifecycle] ?: createJob().let {
+        val newScope = CoroutineScope(it + Dispatchers.Main.immediate)
+        lifecycleCoroutineScopes[lifecycle] = newScope
+        it.invokeOnCompletion { _ -> lifecycleCoroutineScopes -= lifecycle }
+        newScope
+    }
+
+fun LifecycleOwner.uiTask(block: suspend CoroutineScope.() -> Unit) = coroutineScope.launch(block = block)
